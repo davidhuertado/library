e9bf6af96a38f7e3910961c3f8c9e57f5388a50a
David <davidhuertado@gmail.com>
2021-05-25 11:01:18 -0300

    Added displayLibrary function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Array for storing book
 let myLibrary = [];
+const libraryContainer = document.querySelector('.library-container');
 
 const Book = class {
   constructor(title, author, pages, read) {
@@ -19,14 +20,46 @@ const Book = class {
   }
 };
 
-const addBookToLibrary = function (book) {
-  myLibrary.push(book);
+const libraryFunctions = {
+  addBookToLibrary: function (...book) {
+    myLibrary.push(...book);
+  },
+
+  displayLibrary: function () {
+    myLibrary.forEach((book) => {
+      //Create Elements in the DOM
+      let bookDiv = document.createElement('div');
+      let titleElement = document.createElement('h2');
+      let authorElement = document.createElement('h3');
+      let pagesElement = document.createElement('h4');
+      let readItElement = document.createElement('h4');
+
+      //Append elements
+      libraryContainer.appendChild(bookDiv);
+      bookDiv.appendChild(titleElement);
+      bookDiv.appendChild(authorElement);
+      bookDiv.appendChild(pagesElement);
+      bookDiv.appendChild(readItElement);
+
+      //Text of the elements
+      titleElement.textContent = `"${book.title}"`;
+      authorElement.textContent = book.author;
+      pagesElement.textContent = book.pages;
+      book.read
+        ? (readItElement.textContent = 'Read')
+        : (readItElement.textContent = 'Not read yet');
+
+      bookDiv.classList.add('book-container');
+      titleElement.classList.add('book-lines');
+      authorElement.classList.add('book-lines');
+      pagesElement.classList.add('book-lines');
+      readItElement.classList.add('book-lines');
+    });
+  },
 };
 
-const displayLibrary = function () {
-  myLibrary.forEach((book) => {});
-};
-
-// const biblia = new Book('Biliba', 'Jesus', 1000, true);
-// addBookToLibrary(biblia);
-// console.log(myLibrary);
+const biblia = new Book('Biblia', 'Jesus', 1000, false);
+const hobbit = new Book('Hobbit', 'Tolkien', 1500, true);
+libraryFunctions.addBookToLibrary(biblia, hobbit);
+console.log(myLibrary);
+libraryFunctions.displayLibrary();
