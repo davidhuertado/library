87f1dd45b6e5df846b99cc1bfaa002f244ceb97c
David <davidhuertado@gmail.com>
2021-06-04 21:13:59 -0300

    Finished function of delete book and already read?

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+//TODO Terminar funcion de boton borrar
 //Array for storing books
+
 let myLibrary = [];
 let deleteBtns = [];
+let bookIndex = 0;
 const newBookBtn = document.querySelector('.new-book');
 const submitBtn = document.querySelector('.submit-btn');
 const hideCard = document.querySelector('.hide-card');
@@ -38,7 +41,8 @@ const libraryFunctions = {
       //Check if the book is already in the display
       if (book.display) return;
       book.display = true;
-      book.position = myLibrary.length - 1;
+      book.position = bookIndex;
+      bookIndex++;
       //Create Elements in the DOM
       let bookDiv = document.createElement('div');
       let titleElement = document.createElement('h2');
@@ -46,14 +50,23 @@ const libraryFunctions = {
       let pagesElement = document.createElement('h4');
       let readItElement = document.createElement('h4');
       let deleteElement = document.createElement('button');
+      let readBtnElement = undefined;
+      if (book.read === false) {
+        readBtnElement = document.createElement('button');
+      }
 
       //Append elements
-      libraryFunctions.libraryContainer.appendChild(bookDiv);
+
       bookDiv.appendChild(titleElement);
       bookDiv.appendChild(authorElement);
       bookDiv.appendChild(pagesElement);
       bookDiv.appendChild(readItElement);
+      if (readBtnElement !== undefined) {
+        bookDiv.appendChild(readBtnElement);
+      }
+
       bookDiv.appendChild(deleteElement);
+      this.libraryContainer.appendChild(bookDiv);
       //Text of the elements
       titleElement.textContent = `"${book.title}"`;
       authorElement.textContent = book.author;
@@ -61,6 +74,9 @@ const libraryFunctions = {
       book.read
         ? (readItElement.textContent = 'Read')
         : (readItElement.textContent = 'Not read yet');
+      if (readBtnElement !== undefined)
+        readBtnElement.textContent = 'Already read?';
+
       deleteElement.textContent = 'Delete book';
 
       //Atributtes of the elements
@@ -69,8 +85,15 @@ const libraryFunctions = {
       authorElement.classList.add('book-lines');
       pagesElement.classList.add('book-lines');
       readItElement.classList.add('book-lines');
-      deleteElement.classList.add('delete-btn');
-      deleteElement.setAttribute('id', `position-${book.position}`);
+      if (readBtnElement !== undefined) {
+        readBtnElement.classList.add('card-btn', 'read-btn');
+      }
+
+      deleteElement.classList.add('card-btn', 'delete-btn');
+      deleteElement.parentElement.setAttribute(
+        'id',
+        `position-${book.position}`
+      );
     });
   },
   clearCard: function () {
@@ -90,11 +113,35 @@ const formObject = {
 };
 
 //BTN functionality
+
+//Delete BTN
 document
   .querySelector('.library-container')
   .addEventListener('click', function (e) {
     if (e.target.classList.contains('delete-btn')) {
-      e.target.parentElement.remove();
+      const deleteIndex = e.target.parentElement.id.slice(9);
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].position == deleteIndex) {
+          myLibrary.splice(i, 1);
+          e.target.parentElement.remove();
+        }
+      }
+    }
+  });
+
+document
+  .querySelector('.library-container')
+  .addEventListener('click', function (e) {
+    if (e.target.classList.contains('read-btn')) {
+      const index = e.target.parentElement.id.slice(9);
+      for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].position == index) {
+          e.target.parentElement.remove();
+          myLibrary[i].read = true;
+          myLibrary[i].display = false;
+          libraryFunctions.displayLibrary();
+        }
+      }
     }
   });
 newBookBtn.addEventListener('click', function () {
